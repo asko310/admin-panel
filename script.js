cec3968df98679e4028c9a6ce4245908821da87d
asko310 <129404218+asko310@users.noreply.github.com>
2024-06-07 22:41:02 +0300

    Delete + Put thingy

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const formEl = document.querySelector('.form');
+const formEl = document.querySelector('.form-add');
 formEl.addEventListener('submit', event =>{
     event.preventDefault();
     const formData = new FormData(formEl);
@@ -11,6 +11,41 @@ formEl.addEventListener('submit', event =>{
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
+    }).then(res => res.json())
+    .then(data => console.log)
+    .catch(error => console.log(error));
+});
+
+const formPl = document.querySelector('.form-patch');
+formPl.addEventListener('submit', event =>{
+    event.preventDefault();
+    const formData = new FormData(formPl);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+    fetch(`https://vercel-host-ten.vercel.app/products/${data.id}`, {
+        method: 'PUT',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(res => res.json())
+    .then(data => console.log)
+    .catch(error => console.log(error));
+});
+
+const formDl = document.querySelector('.form-patch');
+formDl.addEventListener('submit', event =>{
+    event.preventDefault();
+    const formData = new FormData(formDl);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+    fetch(`https://vercel-host-ten.vercel.app/products/${data.id}`, {
+        method: 'DELETE',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
     }).then(res => res.json())
     .then(data => console.log)
     .catch(error => console.log(error));
